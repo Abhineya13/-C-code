4987313a08e3bc2ccc0d13d881fda5788a4ef612
Abhineya <141350930+Abhineya13@users.noreply.github.com>
2025-11-25 18:25:42 +0000

    Create .c
    Implement a simple program to compare two integers.

---
+++ .c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main(void)
+{
+    // Prompt user for integers
+    int x = get_int("What's x? ");
+    int y = get_int("What's y? ");
+
+    // Compare integers
+    if (x < y)
+    {
+        printf("x is less than y\n");
+    }
+}
